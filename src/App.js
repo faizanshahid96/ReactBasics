723169fd75e661c128eecc0c763dff18ec618d63
Faizan Shahid <faizanshahid0334@gmail.com>
2019-09-18 16:42:30 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -8,15 +8,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code>
+          and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a>
+          className="App-link" href="https://reactjs.org" target="_blank"
+          rel="noopener noreferrer" > Learn React
         </a>
       </header>
     </div>
